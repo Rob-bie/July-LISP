e76fc5cecb9ed806bc3fd39e356b0f3e7c51a085
Rob-bie <deibbor@gmail.com>
2015-10-04 08:30:54 -0400

    Test tokenizing parens and brackets

--- test/july_test.exs
+++ test/july_test.exs
@@ -2,7 +2,12 @@ defmodule JulyTest do
   use ExUnit.Case
   doctest July
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Tokenize parens and brackets" do
+    input = "[()]"
+    output = [{:l_bracket, "[", 1}, {:l_paren, "(", 1},
+              {:r_paren, ")", 1}, {:r_bracket, "]", 1}]
+
+    assert July.Lexer.tokenize(input) == output
   end
+
 end
