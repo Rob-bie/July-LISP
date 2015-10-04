f0cddb8d831ac7cfe37ec733240aa66f1035c58b
Rob-bie <deibbor@gmail.com>
2015-10-04 12:10:18 -0400

    Test tokenizing integers and floats

--- test/july_test.exs
+++ test/july_test.exs
@@ -1,4 +1,5 @@
 defmodule JulyTest do
+
   use ExUnit.Case
   doctest July
 
@@ -13,6 +14,16 @@ defmodule JulyTest do
   test "Tokenize input with various whitespace" do
     input = "    ( \r\t\n )\n"
     output = [{:l_paren, "(", 1}, {:r_paren, ")", 2}]
+
+    assert July.Lexer.tokenize(input) == output
+  end
+
+  test "Tokenize input with integers and floats" do
+    input = "(12\n\n65.323 (12.3))"
+    output = [{:l_paren, "(", 1}, {:integer, "12", 1},
+              {:float, "65.323", 3}, {:l_paren, "(", 3},
+              {:float, "12.3", 3}, {:r_paren, ")", 3},
+              {:r_paren, ")", 3}]
 
     assert July.Lexer.tokenize(input) == output
   end
