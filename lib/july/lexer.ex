93883f3cba384cc78dd99f1e2100c6b726de4aa5
Rob-bie <deibbor@gmail.com>
2015-10-04 12:05:56 -0400

    Tokenizer support for integers and floats

--- lib/july/lexer.ex
+++ lib/july/lexer.ex
@@ -18,43 +18,99 @@ defmodule July.Lexer do
   def tokenize(july_input) do
     july_input
     |> to_char_list
-    |> tokenize([], 1) # Token accumulator, line number
+    |> tokenize([], [], 1) # Token accumulator, tokens, line number
   end
 
-  
   # End of input, return tokens
-  defp tokenize([], token_acc, _) do
-    token_acc |> Enum.reverse
+  defp tokenize([], _, tokens, _) do
+    tokens |> Enum.reverse
   end
 
   # Ingore newline, increment line number
-  defp tokenize([?\n |rest], token_acc, line_number) do
-    tokenize(rest, token_acc, line_number + 1)
+  defp tokenize([?\n |rest], token_acc, tokens, line_number) do
+    tokenize(rest, token_acc, tokens, line_number + 1)
   end
 
   # Ingore all other whitespace
-  defp tokenize([c|rest], token_acc, line_number) when c in '\s\r\t' do
-    tokenize(rest, token_acc, line_number)
+  defp tokenize([c|rest], token_acc, tokens, line_number) when c in '\s\r\t' do
+    tokenize(rest, token_acc, tokens, line_number)
   end
 
   # Accept left paren
-  defp tokenize([?\( |rest], token_acc, line_number) do
-    tokenize(rest, [{:l_paren, "(", line_number}|token_acc], line_number)
+  defp tokenize([?\( |rest], token_acc, tokens, line_number) do
+    tokenize(rest, token_acc, [{:l_paren, "(", line_number}|tokens], line_number)
   end
 
   # Accept right paren
-  defp tokenize([?\) |rest], token_acc, line_number) do
-    tokenize(rest, [{:r_paren, ")", line_number}|token_acc], line_number)
+  defp tokenize([?\) |rest], token_acc, tokens, line_number) do
+    tokenize(rest, token_acc, [{:r_paren, ")", line_number}|tokens], line_number)
   end
 
   # Accept left bracket
-  defp tokenize([?\[ |rest], token_acc, line_number) do
-    tokenize(rest, [{:l_bracket, "[", line_number}|token_acc], line_number)
+  defp tokenize([?\[ |rest], token_acc, tokens, line_number) do
+    tokenize(rest, token_acc, [{:l_bracket, "[", line_number}|tokens], line_number)
   end
 
   # Accept right bracket
-  defp tokenize([?\] |rest], token_acc, line_number) do
-    tokenize(rest, [{:r_bracket, "]", line_number}|token_acc], line_number)
+  defp tokenize([?\] |rest], token_acc, tokens, line_number) do
+    tokenize(rest, token_acc, [{:r_bracket, "]", line_number}|tokens], line_number)
   end
+
+  # Tokenize a number
+  defp tokenize([c|rest], token_acc, tokens, line_number) when c in '0123456789' do
+    integer_digits(rest, [c|token_acc], tokens, line_number)
+  end
+
+  # If a digit is found, prepend it to the token accumulator
+  defp integer_digits([c|rest], token_acc, tokens, line_number) when c in '0123456789' do
+    integer_digits(rest, [c|token_acc], tokens, line_number)
+  end
+
+  # Accept integer, increment line number
+  defp integer_digits([?\n |rest], token_acc, tokens, line_number) do
+    token = get_token(token_acc)
+    tokenize(rest, [], [{:integer, token, line_number}|tokens], line_number + 1)
+  end
+
+  # Accept integer
+  defp integer_digits(chars=[c|_], token_acc, tokens, line_number) when c in '()\s\r\t' do
+    token = get_token(token_acc)
+    tokenize(chars, [], [{:integer, token, line_number}|tokens], line_number)
+  end
+
+  # If a decimal point is found, jump to float state
+  defp integer_digits([c=?\. |rest], token_acc, tokens, line_number) do
+    float_digits(rest, [c|token_acc], tokens, line_number)
+  end
+
+  # Wasn't an integer, jump to symbol state
+  defp integer_digits([c|rest], token_acc, tokens, line_number) do
+    :to_do
+  end
+
+  # If a digit is found, prepend it to the token accumulator
+  defp float_digits([c|rest], token_acc, tokens, line_number) when c in '0123456789' do
+    float_digits(rest, [c|token_acc], tokens, line_number)
+  end
+
+  # Accept float, increment line number
+  defp float_digits([?\n |rest], token_acc, tokens, line_number) do
+    token = get_token(token_acc)
+    tokenize(rest, [], [{:float, token, line_number}|tokens], line_number + 1)
+  end
+
+  # Accept float
+  defp float_digits(chars=[c|_], token_acc, tokens, line_number) when c in '()\s\r\t' do
+    token = get_token(token_acc)
+    tokenize(chars, [], [{:float, token, line_number}|tokens], line_number)
+  end
+
+  # Wasn't a float, jump to symbol state
+  defp float_digits([c|rest], token_acc, tokens, line_number) do
+    :to_do
+  end
+
+  # Convert a token accumulator to a string
+  defp get_token(token_acc), do: Enum.reverse(token_acc) |> to_string
 
 end
