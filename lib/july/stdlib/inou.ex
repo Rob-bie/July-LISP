3319c58291f0e08a78a3fc269b6f735b3ba666e9
Rob-bie <deibbor@gmail.com>
2015-10-21 23:23:28 -0400

    Minor improvements to I/O stdlib

--- lib/july/stdlib/inou.ex
+++ lib/july/stdlib/inou.ex
@@ -2,12 +2,13 @@ defmodule July.Stdlib.Inou do
 
   defp inou do
     %{
-       "read-file" => %{function: &File.read!/1}
-     }
+       "read-file"  => %{function: &File.read!/1},
+       "read-input" => %{function: &(IO.gets(&1) |> String.strip)},
+       "dir?"       => %{function: &Elixir.IEx.Helpers.pwd/0}
+     } 
   end
 
-  def import_inou do
-    inou()
+  def import_inou do    inou()
   end
 
 end
