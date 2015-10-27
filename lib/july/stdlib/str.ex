1fa88e283d0f8a14793fc8d2b692e1ef73525006
Rob-bie <deibbor@gmail.com>
2015-10-26 22:50:58 -0400

    More examples, import structure change, updated escript

--- lib/july/stdlib/str.ex
+++ lib/july/stdlib/str.ex
@@ -5,20 +5,29 @@ defmodule July.Stdlib.Str do
       "split1"     => %{function: &String.split/1},
       "split2"     => %{function: &String.split/2},
       "str->chars" => %{function: &String.codepoints/1},
-      "->lower"    => %{function: &String.downcase/1},
-      "->upper"    => %{function: &String.upcase/1},
+      "str->ascii" => %{function: &to_char_list/1},
+      "ascii->str" => %{function: &List.to_string/1},
+      "str->lower" => %{function: &String.downcase/1},
+      "str->upper" => %{function: &String.upcase/1},
       "contains?"  => %{function: &String.contains?/2},
+      "upper?"     => %{function: &(String.upcase(&1) == &1)},
+      "lower?"     => %{function: &(String.downcase(&1) == &1)},
       "str-len"    => %{function: &String.length/1},
-      "strip"      => %{function: &String.strip/1},
+      "strip1"     => %{function: &String.strip/1},
+      "strip2"     => %{function: &strip_chars/2},
       "replace"    => %{function: &String.replace/3},
-      "str->num"   => %{function: &str_to_num/1},
-     }
+      "str->num"   => %{function: &str_to_num/1}
+     } 
   end
 
   def import_str do
-    str_july_source = File.read!("./lib/july/stdlib/str.july")
+    str_july_source = File.read!("./lib/july/stdlib/julyimpl/str.july")
     {_, str_july_env} = July.Evaluator.eval(str_july_source)
     Dict.merge(str(), str_july_env)
+  end
+
+  defp strip_chars(str, <<c>>) do
+    String.strip(str, c)
   end
 
   defp str_to_num(str) do
